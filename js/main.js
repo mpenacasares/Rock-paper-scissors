1ff70f4b950452025676a32c47584a50e191b8d0
mpenacasares <m.penacasares@gmail.com>
2021-12-14 11:47:08 +0100

    Add JS improvements

--- js/main.js
+++ js/main.js
@@ -1,49 +1,59 @@
 "use strict";
 // elementos HTML
-const valueSelect = document.querySelector('js_select');
-const playBtn = document.querySelector('js_btnPlay');
-const resultText = document.querySelector('js_text');
-const counterUser = document.querySelector('js_user');
-const counterPC = document.querySelector('js_computer');
+const valueSelect = document.querySelector("js_select");
+const playBtn = document.querySelector("js_btnPlay");
+const resultText = document.querySelector("js_text");
+const counterUser = document.querySelector("js_user");
+const counterPC = document.querySelector("js_computer");
 
-
-
-// funciones
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
-
-function computerPlay() {
-    let computerResult = getRandomNumber(10);
-    console.log(computerResult);
-    if (computerResult < 3) {
-        computerResult = "piedra";
-    } else if (computerResult >= 3 && computerOption <= 6) {
-        computerResult = "papel";
-    } else {
-        computerResult = "tijera";
-    }
-    return computerResult;
-  }
-
-  function compareUserOption() {
-    const userOption = valueSelect.value;
-    const computerOption = computerPlay();
-    if (userOption === computerOption()) {
-      console.log(`The number is... ${getRandomNumber()}`);
-    } else {
-      console.log(`try again`);
-    }
-  }
-
-
-
-function handleClickBtn() {
-    // event.preventDefault();
-    computer();
-   
+  return Math.ceil(Math.random() * max);
 }
 
+function changeOptionsGame(resultVariable) {
+  const valueEl = valueSelect.value;
+
+  if (valueEl === resultVariable) {
+    resultText.innerHTML = "Empate";
+  } else if (valueEl === "piedra") {
+    if (resultVariable === "tijera") {
+      resultText.innerHTML = "¡Has ganado!";
+    } else if (resultVariable === "papel") {
+      resultText.innerHTML = "¡Has perdido!";
+    }
+  } else if (valueEl === "papel") {
+    if (resultVariable === "papel") {
+      resultText.innerHTML = "Empate";
+    } else if (resultVariable === "tijera") {
+      resultText.innerHTML = "¡Has perdido!";
+    }
+  } else if (valueEl === "tijera") {
+    if (resultVariable === "piedra") {
+      resultText.innerHTML = "¡Has ganado!";
+    } else if (resultVariable === "papel") {
+      resultText.innerHTML = "¡Has ganado!";
+    }
+  }
+}
+
+function computerPlay() {
+  const randomNum = getRandomNumber(10);
+  let computerResult;
+  if (randomNum < 3) {
+    computerResult = "piedra";
+  } else if (randomNum >= 3 && randomNum <= 6) {
+    computerResult = "papel";
+  } else {
+    computerResult = "tijera";
+  }
+  return computerResult;
+}
+
+function handleClickBtn(event) {
+  event.preventDefault();
+  let resultVariable = computerPlay();
+  changeOptionsGame(resultVariable);
+}
 
 // operaciones
-playBtn.addEventListener('click', handleClickBtn);
+playBtn.addEventListener("click", handleClickBtn);
