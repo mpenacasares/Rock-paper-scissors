f8c2b5165e5e74221d07fed07236d59ec0934bc7
mpenacasares <m.penacasares@gmail.com>
2021-12-19 17:10:39 +0100

    Add moves 10

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ const resultText = document.querySelector(".js_text");
 const counterUser = document.querySelector(".js_user");
 const counterComputer = document.querySelector(".js_computer");
 
-// variablesglobales
+// variables globales
 
 let countUserResult = 0;
 let countComputerResult = 0;
+let moves= 0;
 
 // funciones
 function getRandomNumber(max) {
@@ -23,7 +24,7 @@ function computerPlay() {
   let computerResult="";
   if (randomNum <= 3) {
     computerResult = "piedra";
-  } else if (randomNum>3 && randomNum<= 6) {
+  } else if (randomNum<= 6) {
     computerResult = "papel";
   } else {
     computerResult = "tijera";
@@ -42,32 +43,57 @@ function finalResult() {
 
   } else if (playerSelect === "piedra") {
     if (computerResult === "tijera") {
-      resultText.innerHTML = "¡Has ganado!";
+      resultText.innerHTML = "¡Has ganado!"
+     countUserResult++;
     } else if (computerResult === "papel") {
-      resultText.innerHTML = "¡Has perdido!";
-    }
+      resultText.innerHTML = "¡Has perdido!"
+      countComputerResult++;
 
-  } else if (playerSelect === "papel") {
+    }   else if (playerSelect === "papel") {
     if (computerResult === "piedra") {
-      resultText.innerHTML = "¡Has ganado!";
+      resultText.innerHTML = "¡Has ganado!"
+     countUserResult++;
     } else if (computerResult === "tijera") {
-      resultText.innerHTML = "¡Has perdido!";
+      resultText.innerHTML = "¡Has perdido!"
+      countComputerResult++;
     }
+    
   } else if (playerSelect === "tijera") {
     if (computerResult === "papel") {
-      resultText.innerHTML = "¡Has ganado!";
+      resultText.innerHTML = "¡Has ganado!"
+     countUserResult++;
     } else if (computerResult === "piedra") {
-      resultText.innerHTML = "¡Has perdido!";
+      resultText.innerHTML = "¡Has perdido!"
+      countComputerResult++;
     }
   }
- 
+  }
+  console.log(countComputerResult);
+  console.log(countUserResult);
+
+  counterUser.innerHTML=countUserResult;
+  counterComputer.innerHTML=countComputerResult;
+  moves++;
+  console.log(moves);
 }
 
+// finalizar juego
+function finishGame(){
+  if(moves===10){
+    if(countUserResult===countComputerResult){
+      resultText.innerHTML = "Empate final";
+    }else if(countUserResult>countComputerResult){
+      resultText.innerHTML = "¡Has ganado el juego!";
+    }else{resultText.innerHTML = "¡Has perdido el juego!"}
+  }
+
+}
 
 function handleClickBtn(event) {
   event.preventDefault();
-  finalResult()
+  finalResult();
+  finishGame();
 }
 
 // operaciones
-playBtn.addEventListener('click', handleClickBtn);
+playBtn.addEventListener('click', handleClickBtn)
